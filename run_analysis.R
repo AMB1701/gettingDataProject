8091e8076d688a14744b8189a0a59535525935cd
Andrew Burke <amb_1701@yahoo.com>
2015-04-26 19:39:47 -0400

    no message

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#load and merge the training and testing sets
+#Phase 1: load and merge the training and testing sets
 xValues <- rbind(read.table("train/X_train.txt"), read.table("test/X_test.txt"))
 yValues <- rbind(read.table("train/y_train.txt"), read.table("test/y_test.txt"))
 subjValues <- rbind(read.table("train/subject_train.txt"), read.table("test/subject_test.txt"))
@@ -7,29 +7,29 @@ colnames(xValues) <- tolower(gsub("\\(\\)","",allFeatures[,2]))
 colnames(yValues) <- c("activity-name")
 colnames(subjValues) <- c("subject")
 
-#select only the mean and standard deviation entries (from features.txt)
+#Phase 2: select only the mean and standard deviation entries (from features.txt)
 meanIndices <- grep("-mean",allFeatures[,2])
 stdIndices <- grep("-std",allFeatures[,2])
 allIndices <- c(meanIndices,stdIndices)
 xValues <- xValues[,allIndices]
 
-#rename the activities using the descriptive labels (from activity_labels.txt)
+#Phase 3: rename the activities using the descriptive labels (from activity_labels.txt)
 allActivities <- read.table("activity_labels.txt")
 allActivities[,2] <- tolower(allActivities[,2])
 #reassign names in yValues using indicies in allActivities
 yValues[,1] <- allActivities[yValues[,1],2]
 
-#create original output set
+#Phase 4: create original output set
 tidySet <- cbind(subjValues,yValues,xValues)
 write.table(tidySet,"tidy_set_original.txt")
 
-#calculate averages for each variable for each activity and subject
+#Phase 5: calculate averages for each variable for each activity and subject
 uniqueSubjects <- unique(subjValues[,1])
 finalSet <- tidySet[1:(length(uniqueSubjects)*length(allActivities[,1])),]
 
 currentIndex <- 1
 
-#for each subject...
+#Phase 6: for each subject...
 for(subjectID in 1:length(uniqueSubjects))
 {
   #for each activity...
@@ -44,5 +44,5 @@ for(subjectID in 1:length(uniqueSubjects))
   }
 }
 
-#create final output set with calculations
+#Phase 7: create final output set with calculations
 write.table(finalSet,"tidy_set_averages.txt", row.name=FALSE)
